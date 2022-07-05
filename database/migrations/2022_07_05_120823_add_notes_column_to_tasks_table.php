dfd5fc327f9236169fab317516803ddc47c47e67
MuresanSebastian <muresan.sebastian12@yahoo.com>
2022-07-05 17:49:42 +0300

    migration for new column

---
+++ database/migrations/2022_07_05_120823_add_notes_column_to_tasks_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNotesColumnToTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->string('notes', 255)->after('name')->nullable()->default(NULL);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
